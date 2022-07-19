54bc86b96b6ecda659f6227e4c8dde028d1af715
Laurynas Ruškys <bilbas@gmail.com>
2022-07-20 00:33:29 +0300

    HA discovery

--- main.py
+++ main.py
@@ -9,10 +9,39 @@ import usyslog
 import ntptime
 import time
 import webrepl
+import json
 
 client_id = 'cripple'
 
-log = usyslog.SyslogClient()
+class PrintClient(usyslog.SyslogClient):
+    def __init__(self):
+        super().__init__()
+
+    def log(self, severity, msg):
+        data = "<%d>%s" % (severity + (self._facility << 3), msg)
+        print(data)
+        
+    def close(self):
+        pass
+
+
+
+class CombineClient(usyslog.SyslogClient):
+    def __init__(self, loggers):
+        self.loggers = loggers
+        super().__init__()
+
+    def log(self, severity, msg):
+        for log in self.loggers:
+            log.log(severity, msg)
+
+    def close(self):
+        pass
+
+
+
+log = PrintClient()
+
 
 def blink(times=1):
     led = Pin(2, Pin.OUT)
@@ -22,7 +51,11 @@ def blink(times=1):
         led.on()
         utime.sleep_ms(200)
 
+
+
 blink(1)
+
+
 
 def init_network():
     nic = network.WLAN(network.STA_IF)
@@ -38,39 +71,125 @@ def init_network():
     client_id = nic.config('dhcp_hostname')
 
 
+
 def get_logger():
     global log
-    log = usyslog.UDPClient('libreelec.lan')
+    log = CombineClient([usyslog.UDPClient('nas.lan'), PrintClient()])
 
 
-def report_sensors():
+
+def connect_mqtt():
+    mqtt = MQTTClient(client_id, 'nas.lan', keepalive=60)
+    mqtt.set_last_will('{}/state'.format(client_id), 'offline', retain=True)
+    mqtt.connect()
+    log.info('MQTT connected')
+
+    mqtt.publish(
+        '{}/state'.format(client_id),
+        'online', retain=False)
+
+    return mqtt
+
+
+
+def publish_config(mqtt):
+    device = {
+        "manufacturer": "bilbas",
+        "model": "esp8266-bme280",
+        "identifiers": [
+            client_id
+        ]
+    }
+
+    temp_topic = 'homeassistant/sensor/{}/temperature/config'.format(client_id)
+    temp_config = {
+        "device": device,
+        "~": client_id,
+        "unique_id": "{}_temp".format(client_id),
+        "device_class": "temperature",
+        "name": "Temperature",
+        "state_topic": "~/temperature",
+        "unit_of_measurement": "°C"
+    }
+
+    mqtt.publish(
+        temp_topic,
+        json.dumps(temp_config), retain=True)
+
+
+    pressure_topic = 'homeassistant/sensor/{}/pressure/config'.format(
+        client_id)
+    pressure_config = {
+        "device": device,
+        "~": client_id,
+        "unique_id": "{}_pressure".format(client_id),
+        "device_class": "pressure",
+        "name": "Pressure",
+        "state_topic": "~/pressure",
+        "unit_of_measurement": "hPa",
+        "availability_topic": "~/state"
+    }
+
+    mqtt.publish(
+        pressure_topic,
+        json.dumps(pressure_config), retain=True)
+
+
+    humidity_topic = 'homeassistant/sensor/{}/humidity/config'.format(
+        client_id)
+    humidity_config = {
+        "device": device,
+        "~": client_id,
+        "unique_id": "{}_humidity".format(client_id),
+        "device_class": "humidity",
+        "name": "Humidity",
+        "state_topic": "~/humidity",
+        "unit_of_measurement": "%",
+        "availability_topic": "~/state"
+    }
+
+    mqtt.publish(
+        humidity_topic,
+        json.dumps(humidity_config), retain=True)
+
+
+    battery_topic = 'homeassistant/sensor/{}/battery/config'.format(client_id)
+    battery_config = {
+        "device": device,
+        "~": client_id,
+        "unique_id": "{}_voltage".format(client_id),
+        "device_class": "battery",
+        "name": "Battery",
+        "state_topic": "~/voltage",
+        "unit_of_measurement": "V",
+        "availability_topic": "~/state"
+    }
+
+    mqtt.publish(
+        battery_topic,
+        json.dumps(battery_config), retain=True)
+
+
+
+def report_sensors(mqtt):
     i2c = I2C(scl=Pin(5), sda=Pin(4), freq=10000)
     bme = BME280(i2c=i2c)
 
     (temp, pressure, humidity) = bme.read_compensated_data()
 
-    mqtt = MQTTClient(client_id, 'nas.lan', keepalive=60)
-    mqtt.set_last_will('{}/status'.format(client_id), 'offline', retain = True)
-    mqtt.connect()
-    log.info('MQTT connected')
-
-    mqtt.publish(
-        '{}/status'.format(client_id),
-        'connected', retain = False)
-
     mqtt.publish(
         '{}/temperature'.format(client_id),
-        str(temp/100), retain = False)
+        str(temp/100), retain=False)
     log.info('temp: {}'.format(temp/100))
 
     mqtt.publish(
         '{}/pressure'.format(client_id),
-        str(pressure/25600), retain = False)
+        str(pressure/25600), retain=False)
     log.info('pres: {}'.format(pressure/25600))
 
     mqtt.publish(
         '{}/humidity'.format(client_id),
-        str(humidity/1024), retain = False)
+        str(humidity/1024), retain=False)
     log.info('humi: {}'.format(humidity/1024))
 
     adc = ADC(0)
@@ -78,20 +197,19 @@ def report_sensors():
     volts = volts_reading/239.0
 
     mqtt.publish(
-        '{}/voltage'.format(client_id), str(volts), retain = False)
+        '{}/voltage'.format(client_id), str(volts), retain=False)
     log.info('volt: {}'.format(volts))
 
     mqtt.publish(
-        '{}/status'.format(client_id), 'disconnected', retain = True)
+        '{}/status'.format(client_id), 'disconnected', retain=True)
 
-    mqtt.disconnect()
-    log.info('MQTT disconnected')
 
 
 def set_sleep(sleep):
     rtc = machine.RTC()
     mem = b'{}'.format(sleep)
     rtc.memory(mem)
+
 
 
 def go_to_sleep(force=True):
@@ -117,6 +235,8 @@ def go_to_sleep(force=True):
         log.info('going deep sleep')
         esp.deepsleep(sleep)
 
+
+
 def main():
     blink(2)
     try:
@@ -129,14 +249,22 @@ def main():
 
         localtime = time.localtime()
         log.info('init {}'.format(localtime))
-        
+
         reset_cause = machine.reset_cause()
         log.info("Reset cause: %s" % reset_cause)
         blink(3)
-        report_sensors()
+
+        mqtt = connect_mqtt()
+        publish_config(mqtt)
+        report_sensors(mqtt)
+        mqtt.disconnect()
+        log.info('MQTT disconnected')
+
         blink(4)
         go_to_sleep(False)
     except Exception as e:
         log.error("Error: %s" % e)
+
+
 
 main()
